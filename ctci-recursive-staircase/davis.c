10a212e895af6c97fe9a89aceab190d73925fa0a
abhishek sansanwal <donotreply@dummy.com>
2017-08-17 22:38:32 +0530

    added memoization to recursive step

--- ctci-recursive-staircase/davis.c
+++ ctci-recursive-staircase/davis.c
@@ -1,17 +1,24 @@
 #include<iostream>
 using namespace std;
+
+int memoize[37];
+
 int steps(int s)
 {
 if(s==0)
 	return 1;
 if(s<0)
 	return 0;
-return steps(s-1) + steps(s-2) + steps(s-3);
+if(memoize[s]!=-1)
+	return memoize[s];
+memoize[s] = steps(s-1) + steps(s-2) + steps(s-3);
+return memoize[s];
 }
 
 int main() {
 	int s;
 	cin>>s;
+	for(int i=0;i<37;i++) memoize[i]= -1;
 	while(s--){
 	int t;
 	cin>>t;
